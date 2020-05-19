80e137ca10c82f3fa4e0e6f4e42a9f0ce12a6380
Maxime Schellenberger <maxschell31@gmail.com>
2020-05-19 14:24:07 +0200

    slider

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// Charge le DOM et démarre le slider
+window.addEventListener("DOMContentLoaded", () => {
+    slideShow()
+});
+
+// Création du slider
+function slideShow(){
+    
+    // On stock les photos du slider dans un array
+    const pic = ["img/surf-1.jpg","img/surf-2.jpg","img/surf-3.jpg","img/surf-4.jpg","img/surf-5.jpg"]
+
+    // On sélectionne la balise img
+    let img = document.getElementsByTagName("img")[0]
+
+    let count = -1;
+
+    function showSlide() {
+
+        if(count >= pic.length-1){
+            count = -1;
+        }
+
+        count++;
+
+        img.setAttribute("src",pic[count]); // On change l'attribut src de la balise img
+
+        setTimeout(showSlide,2000) // Tous les 2 secs
+    }
+
+    showSlide()
+
+}
+
+
+
+
